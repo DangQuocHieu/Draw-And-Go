bf4af7402056033fb50be860e777c1d24b8e4695
DangQuocHieu <162134324+DangQuocHieu@users.noreply.github.com>
2025-06-22 16:49:41 +0700

    Add car controller

--- Assets/_Scripts/Gameplay/Line.cs
+++ Assets/_Scripts/Gameplay/Line.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Line : MonoBehaviour
 {
     private LineRenderer _lineRenderer;
+    private EdgeCollider2D _edgeCollider;
+    private List<Vector2> _allPoints = new List<Vector2>();
     void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
         _lineRenderer.positionCount = 0;
+        _edgeCollider = GetComponent<EdgeCollider2D>();
     }
 
     public void SetPosition(Vector2 position, float drawResolution)
@@ -14,6 +18,9 @@ public class Line : MonoBehaviour
         if (!CanAppend(position, drawResolution)) return;
         ++_lineRenderer.positionCount;
         _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, position);
+        _allPoints.Add(transform.InverseTransformPoint(position));
+        _edgeCollider.points = _allPoints.ToArray();
+        
     }
 
     private bool CanAppend(Vector2 position, float drawResolution)
